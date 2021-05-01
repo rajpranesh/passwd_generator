125ef79d102fcf62c3fc608ce7e04569be3d7c52
rajpranesh <70705173+rajpranesh@users.noreply.github.com>
2021-05-01 15:17:03 +0530

    Add files via upload

---
+++ random_password_gen.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+
+import random
+import string
+
+# printing lowercase
+letters = string.ascii_lowercase
+print ( ''.join(random.choice(letters) for i in range(10)) )
+
+uppercase=a b c d e f g h i j k l m n o p q r s t u v w x y z
